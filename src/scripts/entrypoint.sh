dd3d4c4f64ebd3e25c3a9997ae4ce3ad47a26e1a
Sebastian Pietras <me@spietras.dev>
2024-03-09 19:23:02 +0100

    Cleaned up venv management

--- src/scripts/entrypoint.sh
+++ src/scripts/entrypoint.sh
@@ -7,12 +7,6 @@
 # Set permissions
 chown -R app: /app/
 
-# Activate virtual environment
-# shellcheck source=/dev/null
-. .venv/bin/activate
-
-export PYTHONPATH="${VIRTUAL_ENV:?}/${PYTHON_SITE_PACKAGES:?}:${PYTHONPATH:-}"
-
 # Run as non-root user
 # Use tini to handle signals
 exec su-exec app tini -- "$@"
--- src/scripts/shell.sh
+++ src/scripts/shell.sh
@@ -4,10 +4,4 @@
 # shellcheck source=/dev/null
 . /env/activate
 
-# Activate virtual environment
-# shellcheck source=/dev/null
-. .venv/bin/activate
-
-export PYTHONPATH="${VIRTUAL_ENV:?}/${PYTHON_SITE_PACKAGES:?}:${PYTHONPATH:-}"
-
 /bin/bash -c "$*"
