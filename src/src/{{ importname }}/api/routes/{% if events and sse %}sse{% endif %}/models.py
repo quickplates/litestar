777cf3b4a6a65e8a4a7e00e38dc8ad1d8fb43085
Sebastian Pietras <me@spietras.dev>
2024-07-26 23:44:31 +0200

    Cleaned up code (#29)

--- src/src/{{ importname }}/api/routes/{% if events and sse %}sse{% endif %}/models.py
+++ src/src/{{ importname }}/api/routes/{% if events and sse %}sse{% endif %}/models.py
@@ -1,1 +1,16 @@
-SubscribeMessage = str
+from collections.abc import AsyncIterator
+from dataclasses import dataclass
+
+
+@dataclass(kw_only=True)
+class SubscribeRequest:
+    """Request to subscribe."""
+
+    pass
+
+
+@dataclass(kw_only=True)
+class SubscribeResponse:
+    """Response for subscribe."""
+
+    messages: AsyncIterator[str]
