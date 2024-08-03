3822159cadcc69aff7e8467e95db072fd4966a9c
Sebastian Pietras <me@spietras.dev>
2024-08-04 00:31:05 +0200

    Added `HEAD` method support to ping endpoint (#39)

--- src/tests/integration/test_ping.py
+++ src/tests/integration/test_ping.py
@@ -18,3 +18,20 @@ async def test_get(client: AsyncTestClient) -> None:
 
     content = response.content
     assert len(content) == 0
+
+
+@pytest.mark.asyncio(scope="session")
+async def test_head(client: AsyncTestClient) -> None:
+    """Test if HEAD /ping returns correct response."""
+
+    response = await client.head("/ping")
+
+    status = response.status_code
+    assert status == HTTP_204_NO_CONTENT
+
+    headers = response.headers
+    assert "Cache-Control" in headers
+    assert headers["Cache-Control"] == "no-store"
+
+    content = response.content
+    assert len(content) == 0
