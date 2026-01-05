3bf6a2686c8bad5898110f8f8d62c680aaef404b
Sebastian Pietras <me@spietras.dev>
2026-01-05 13:21:57 +0100

    Cleaned up code

--- src/src/{{ importname }}/utils/time.py
+++ src/src/{{ importname }}/utils/time.py
@@ -1,5 +1,10 @@
 from datetime import UTC, datetime
 from email.utils import format_datetime, parsedate_to_datetime
+from typing import Annotated
+from zoneinfo import ZoneInfo, ZoneInfoNotFoundError
+
+from pydantic import AfterValidator, Field, TypeAdapter
+from pydantic import NaiveDatetime as PydanticNaiveDatetime
 
 
 def awareutcnow() -> datetime:
@@ -25,3 +30,39 @@ def httpparse(value: str) -> datetime:
 def httpstringify(dt: datetime) -> str:
     """Convert a datetime to an HTTP date string."""
     return format_datetime(dt, usegmt=True)
+
+
+class TimezoneValidationError(ValueError):
+    """Timezone validation error."""
+
+    def __init__(self, value: str) -> None:
+        super().__init__(f"Invalid time zone: {value}")
+
+
+def validate_timezone(value: str) -> str:
+    """Validate a time zone."""
+    try:
+        ZoneInfo(value)
+    except ZoneInfoNotFoundError as e:
+        raise TimezoneValidationError(value) from e
+
+    return value
+
+
+Timezone = Annotated[
+    str,
+    AfterValidator(validate_timezone),
+    Field(examples=["Europe/Warsaw"]),
+]
+
+
+def validate_naive_datetime(value: datetime) -> datetime:
+    """Validate a naive datetime."""
+    return TypeAdapter(PydanticNaiveDatetime).validate_python(value)
+
+
+NaiveDatetime = Annotated[
+    datetime,
+    AfterValidator(validate_naive_datetime),
+    Field(examples=["2025-01-01T00:00:00"]),
+]
